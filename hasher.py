d9cd6b5fd7f0912e6424c05b946afb5782313f81
EveningPerson <65233561+EveningPerson@users.noreply.github.com>
2020-10-28 16:25:33 +0100

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -1,10 +1,21 @@
 import hashlib
 
-print("MD5 Hasher")
-print("By 3v3n1ng")
+author = """
+  ______               _             _____                          
+ |  ____|             (_)           |  __ \                         
+ | |____   _____ _ __  _ _ __   __ _| |__) |__ _ __ ___  ___  _ __  
+ |  __\ \ / / _ \ '_ \| | '_ \ / _` |  ___/ _ \ '__/ __|/ _ \| '_ \ 
+ | |___\ V /  __/ | | | | | | | (_| | |  |  __/ |  \__ \ (_) | | | |
+ |______\_/ \___|_| |_|_|_| |_|\__, |_|   \___|_|  |___/\___/|_| |_|
+                                __/ |                               
+                               |___/                                """
+print(author)
+print("MD5Hasher")
 
 # Input
+print("")
 word = input('Enter Word to hash: ')
 
 hash_object = hashlib.md5(word.encode())
 print(hash_object.hexdigest())
+
